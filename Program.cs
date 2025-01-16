42efc05f892543089e8cfa39a891b7944df6772a
Ryan Roethle <ryanroethle7474@gmail.com>
2025-01-16 16:45:13 -0600

    Added initial azure-pipelines file.
    Allowed swagger for both test and production.

--- Program.cs
+++ Program.cs
@@ -177,12 +177,8 @@ using (var scope = app.Services.CreateScope())
     await CreateRoles(roleManager);
 }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
